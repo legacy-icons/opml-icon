cc3765a21c8a7174f73c75f1246e67f5ba98f51c
t1st3 <contact@tiste.org>
2016-01-16 20:02:10 +0100

    chore: remove figlets, cowsays and notifications

--- gulpfile.js
+++ gulpfile.js
@@ -10,55 +10,9 @@ del = require('del'),
 somebody = require('somebody'),
 pkgAuthor = somebody.parse(pkg.author),
 
-figletShown = 0,
-figlet = require('figlet'),
-cowsay = require('cowsay'),
 qrcode = require('qrcode-terminal'),
 
-gulp = require('gulp'),
-notify = require('gulp-notify');
-
-function displayCowsay (txt, cb) {
-  console.log('\n\n');
-  console.log(chalk.magenta(cowsay.say({
-    text: pkg.name + ' - ' + txt,
-    e: 'oO',
-    T: 'U '
-  })));
-  console.log('\n\n');
-  cb();
-}
-
-function triggerNotification (title, txt, cb) {
-  gulp.src('./')
-    .pipe(notify({
-      title: pkg.name + ' - ' + title,
-      message: txt
-    }));
-  cb();
-}
-
-gulp.task('figlet', [], function (cb) {
-  if (figletShown === 0) {
-    figlet.text(pkg.name, {
-      font: 'Small',
-      horizontalLayout: 'default',
-      verticalLayout: 'default'
-    }, function(err, data) {
-      if (err) {
-        console.log('Something went wrong with FIGlet');
-        console.dir(err);
-        return;
-      }
-      console.log('\n\n');
-      console.log(chalk.green(data));
-      console.log(chalk.blue(pkg.version));
-      console.log('\n\n');
-      figletShown = 1;
-      cb();
-    });
-  }
-});
+gulp = require('gulp');
 
 /*
  * INFO TASK
